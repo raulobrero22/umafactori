6b875d804fa479b911df0519ecabef203fe154ff
raulobrero22 <raulobreroberlanga@uma.es>
2023-02-04 18:53:25 +0100

    implementacion recursiva

--- unafactori.java
+++ unafactori.java
@@ -2,20 +2,11 @@ package unafactori;
 
 import java.util.Scanner;
 
-public class unafactori {
-	public static long fact(long num) {
-		long fac = 1;
-		for (long i=1; i<=num; ++i) {
-		fac = fac * i;
-		}
-		return fac;
-		}
-		public static void main(String []args) {
-		Scanner scanInput = new Scanner(System.in);
-		System.out.print("Introduzca un numero: ");
-		long num = scanInput.nextLong();
-		long fac = fact(num);
-		System.out.println("El factorial de "+num+" es "+fac);
-		scanInput.close();
-		}
+public static long fact(long num) {
+	if (num<=0) {
+	return 1;
+	} else {
+	return num*fact(num-1);
+	}
+	}
 }
