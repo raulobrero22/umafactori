812f923ce865bdebdb849fd0986f01afb38e0b5e
raulobrero22 <raulobreroberlanga@uma.es>
2023-02-04 19:07:55 +0100

    correcion numeros negativos

--- unafactori.java
+++ unafactori.java
@@ -11,12 +11,17 @@ public class unafactori {
 		return fac;
 		}
 		public static void main(String []args) {
-		Scanner scanInput = new Scanner(System.in);
-		System.out.print("Introduzca un numero: ");
-		long num = scanInput.nextLong();
-		long fac = fact(num);
-		System.out.println("El factorial de "+num+" es "+fac);
-		scanInput.close();
+			Scanner scanInput = new Scanner(System.in);
+			System.out.print("Introduzca un numero: ");
+			long num = scanInput.nextLong();
+			if (num<0) {
+			System.out.println("El factorial no esta definido"
+			+ " para numeros negativos");
+			} else {
+			long fac = fact(num);
+			System.out.println("El factorial de "+num+" es "+fac);
+			}
+			scanInput.close();
 		}
 		
 }
